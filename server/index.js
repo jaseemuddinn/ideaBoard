debf2700026a1375eab6c43eed9f9bcd7bd22e9f
Jaseemuddin Naseem <jaseemuddinn@gmail.com>
2025-04-23 00:05:58 +0530

    dbconnect removed

--- server/index.js
+++ server/index.js
@@ -4,8 +4,6 @@ import { connectDB } from './db';
 const app = express();
 const PORT = 3000;
 
-// connectDB();
-// app.use(express.json())
 app.listen(PORT, () => {
     console.log("Server is running.")
 })
